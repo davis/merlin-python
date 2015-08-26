95010155590340a3845a4d30d48f9dfe19849fb6
Andrew Stanton <refefer@gmail.com>
2015-08-26 10:08:29 -0700

    Init

---
+++ setup.py
@@ -0,0 +1,17 @@
+# !/usr/bin/env python
+
+from setuptools import setup
+from distutils.core import setup
+
+import merlin
+
+setup(name='py-merlin',
+      version=merlin.__version__,
+      description='Python bindings for Blackbird Search',
+      url="https://github.com/blackbirdtech/merlin-python",
+      packages=['merlin'],
+      scripts=[],
+      install_requires=[
+          # See requirements.txt
+      ],
+      author='Blackbird Technologies')
