fd20f10cec508c68342713e8c4c40ae96f1ec28b
Andrew Stanton <refefer@gmail.com>
2015-12-04 13:50:19 -0800

    Bumping version

--- merlin/__init__.py
+++ merlin/__init__.py
@@ -1,5 +1,5 @@
 from urlparse import ParseResult, urlunparse
-__version__ = '0.1.0'
+__version__ = '0.2.0'
 
 from .common import Api, Engine, DefaultEngine, UploadEngine
 from .upload import IndexOp
